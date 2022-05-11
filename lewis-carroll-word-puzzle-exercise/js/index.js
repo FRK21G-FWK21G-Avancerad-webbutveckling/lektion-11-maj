09e1ca05dc6d8804584293a417543f64ffa4ffdb
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2022-05-11 10:51:36 +0200

    Added code

--- lewis-carroll-word-puzzle-exercise/js/index.js
+++ lewis-carroll-word-puzzle-exercise/js/index.js
@@ -14,6 +14,7 @@ const startWordElem = document.querySelector('#start-word');
 const endWordElem = document.querySelector('#end-word');
 const inputElem = document.querySelector('#input-word');
 const buttonElem = document.querySelector('#submit-word');
+const wordsElem = document.querySelector('#words');
 
 function startGame() {
     // Slumpar en position i arrayen
@@ -30,8 +31,6 @@ function onlyChangedOneLetter(newWord, lastWord) {
     for(let i = 0; i < newWord.length; i++) {
         if (newWord[i] !== lastWord[i]) {
             changes++;
-            console.log('Nytt ord: ', newWord[i]);
-            console.log('Start ord: ', lastWord[i]);
         }
     }
 
@@ -42,19 +41,56 @@ function onlyChangedOneLetter(newWord, lastWord) {
     }
 }
 
-buttonElem.addEventListener('click', () => {
+async function isAEnglishWord(newWord) {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${newWord}`);
+    const data = await response.json();
+
+    console.log(data);
+    if (data.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function hasWon(lastWord) {
+    if (lastWord === puzzle.endWord) alert('Du vann!');
+}
+
+function createWordElem(word) {
+    const wordElem = document.createElement('li');
+    wordElem.innerHTML = word;
+    wordsElem.append(wordElem);
+}
+
+function displayWords() {
+    wordsElem.innerHTML = ''; // Tar bort alla li-taggar i vår ul
+
+    for(let i = 0; i < changedWords.length; i++) {
+        createWordElem(changedWords[i]);
+    }
+}
+
+buttonElem.addEventListener('click', async () => {
     const newWord = inputElem.value;
     let correctChange;
+    let correctEnglishWord;
 
-    if (changedWords.length > 0) {
-        const lastWord = changedWords[changedWords.length - 1]; // Plockar ut sista ordet i arrayen
-        correctChange = onlyChangedOneLetter(newWord, lastWord);
-    } else {
-        correctChange = onlyChangedOneLetter(newWord, puzzle.startWord);
+    if (newWord.length === puzzle.startWord.length) {
+        if (changedWords.length > 0) {
+            const lastWord = changedWords[changedWords.length - 1]; // Plockar ut sista ordet i arrayen
+            correctChange = onlyChangedOneLetter(newWord, lastWord);
+            correctEnglishWord = await isAEnglishWord(newWord);
+        } else {
+            correctChange = onlyChangedOneLetter(newWord, puzzle.startWord);
+            correctEnglishWord = await isAEnglishWord(newWord);
+        }
     }
     
-    if (correctChange) {
+    if (correctChange && correctEnglishWord) {
         changedWords.push(newWord);
+        hasWon(newWord);
+        displayWords();
         console.log(changedWords);
     } else {
         console.log('Du fuskar!');
